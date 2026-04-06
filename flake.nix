ef3ec8ca6be649f3ec6b517e2e79835303ee3153
mib <mib@kanp.ai>
2026-04-06 19:46:32 +0200

    nix devshell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    conch = {
+      url = "github:mibmo/conch";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { conch, ... }:
+    conch.load (
+      { pkgs, ... }:
+      {
+        shell.packages = with pkgs; [
+          act
+        ];
+      }
+    );
+}
